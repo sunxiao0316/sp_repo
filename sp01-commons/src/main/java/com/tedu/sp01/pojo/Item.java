4fcebe22e408263147fd73dfa41898dac55a8b2f
sunxiao0316 <770356526@qq.com>
2019-06-27 17:02:50 +0800

    1

---
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -0,0 +1,18 @@
+package com.tedu.sp01.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Item {
+	private Integer id;
+	private String name;
+	private Integer number;
+}
+
